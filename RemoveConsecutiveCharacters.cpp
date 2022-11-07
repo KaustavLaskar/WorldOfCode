7621f9438b046662aba854df0550f8d7ccfc7ce0
Kaustav Laskar <109295782+KaustavLaskar@users.noreply.github.com>
2022-11-07 23:45:41 +0530

    Add files via upload

---
+++ RemoveConsecutiveCharacters.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+void removeConsecutiveCharecter(char*s)
+{
+    if(s[0]=='\0')
+    {
+        return;
+    }
+    if(s[0]==s[1])
+    {
+        int i=0;
+        while (s[i]!='\0')
+        {
+            s[i]=s[i+1];
+            i++;
+        }
+        removeConsecutiveCharecter(s);
+    }
+    removeConsecutiveCharecter(s+1);
+}
+
+
+int main()
+{
+    char S1[] = "geeksforgeeks";
+    removeConsecutiveCharecter(S1);
+    cout << S1 << endl;
+    char S2[] = "aabcca";
+    removeConsecutiveCharecter(S2);
+    cout << S2 << endl;
+    return 0;
+}
